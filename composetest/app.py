82412af8f79fe8c823c915e5c86d22425c858479
namickey <teethdai@gmail.com>
2018-12-24 22:55:24 +0900

    add scale training.

---
+++ composetest/app.py
@@ -0,0 +1,45 @@
+import time
+import redis
+from flask import Flask, session
+import uuid
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    name = 'none'
+    if 'user' in session:
+        name = cache.hget(str(session['user']), 'name')
+        if name:
+            name = name.decode()
+        else:
+            name = genUuid()
+    else:
+        name = genUuid()
+    return str(session['user']) +'<br> Hello ' + name + '!!<br> I have been seen {} times.\n'.format(count)
+
+def genUuid():
+    id = str(uuid.uuid4())
+    session['user'] = id
+    name = 'hoge12345'
+    cache.hset(id, 'name', name)
+    return name
+
+app.secret_key = 'abcd'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ composetest/scale.sh
@@ -0,0 +1,1 @@
+sudo docker-compose up --scale web=3

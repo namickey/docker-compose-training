d3e6124b4bce62bd557d6cacfa8273da70269192
namickey <teethdai@gmail.com>
2019-03-03 11:49:55 +0900

    add map.

---
+++ gcp/flask/map.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import logging
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/getmap', methods=['GET'])
+def getmap():
+    res = jsonify({"point":[\
+        {"x":35.69, "y":139.7},\
+        {"x":35.61, "y":139.6}\
+        ]})
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500

7343eb163d06d83446e473c59b2a96cf908bde1a
namickey <teethdai@gmail.com>
2019-03-03 15:26:29 +0900

    add html

--- gcp/flask/map.py
+++ gcp/flask/map.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -11,6 +11,10 @@ app = Flask(__name__)
 
 scopes = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('mapkey.json', scopes)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('map.html')
 
 @app.route('/getmap', methods=['GET'])
 def getmap():
